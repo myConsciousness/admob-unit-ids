80fac0ee0b01a60d3cc4da1179c0ee7f2062d120
myConsciousness <kato.shinya.dev@gmail.com>
2021-12-28 13:30:07 +0900

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,15 @@
-### 1.0.2
+# Release Note
+
+## 1.0.2
 
 - Fixed some documents.
+- Fixed the dependency.
 
-### 1.0.1
+## 1.0.1
 
 - Support cross platform test unit ids.
 
-### 1.0.0
+## 1.0.0
 
 - Official release.
 
--- lib/src/admob_unit_ids.dart
+++ lib/src/admob_unit_ids.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
+import 'package:universal_io/io.dart';
 
 /// The abstract class that represents the manager of AdMob Unit ID.
 ///
